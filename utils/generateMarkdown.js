150087895e054c69f67520dee41c6789d774e4f2
KatyKedi <katykat5bros@gmail.com>
2022-02-23 14:46:45 -0800

    fix table of contents links

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -77,12 +77,12 @@ function generateMarkdown(readmeData) {
   ${readmeData.description}
 
   ## Table of Contents
-  1. [Installation](.#installation)
-  2. [Usage](.#usage)
-  3. [License](.#license)
-  4. [Contributing](.#contributing)
-  5. [Tests](.#tests)
-  6. [Questions](.#questions)
+  1. [Installation](#installation)
+  2. [Usage](#usage)
+  3. [License](#license)
+  4. [Contributing](#contributing)
+  5. [Tests](#tests)
+  6. [Questions](#questions)
 
   ## Installation
   ${readmeData.installation}
